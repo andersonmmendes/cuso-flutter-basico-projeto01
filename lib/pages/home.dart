f116d28965d5f08514080457f43288da8f93548e
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-04-23 18:32:53 -0300

    Aula 18 - Finalizada

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:projeto01/controllers/home_controller.dart';
+import 'package:projeto01/models/post_model.dart';
+import 'package:projeto01/repositories/home_repository_mock.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final HomeController _controller = HomeController(HomeRepositoryMock());
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.fetch();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ValueListenableBuilder<List<PostModel>>(
+        valueListenable: _controller.posts,
+        builder: (_, list, __) {
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (_, index) => ListTile(
+              title: Text(list[index].title),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
